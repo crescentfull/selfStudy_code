e741d0e3dc7a692eb2e0507f9ae2c71cf3e0d51a
song <song7868@gmail.com>
2022-01-11 18:00:35 +0900

    [ADD] level1-3 solve

--- Algorithm/programmers/level01/level1-3.py
+++ Algorithm/programmers/level01/level1-3.py
@@ -1,4 +1,5 @@
 '''
+[짝수와 홀수]
 ## 문제 설명
 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
 
@@ -16,6 +17,7 @@ num = 3
 num2 = 4
 def solution(num):
     answer = ''
+    answer = 'Even' if num%2==0 else 'Odd'
     return answer
 
 print(solution(num))
