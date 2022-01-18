e53e6dbf18176980c7ee604e3a6ee816a76b0c8f
song <song7868@gmail.com>
2022-01-18 20:27:48 +0900

    ADD: lifecoding initial setting

--- TIL/django/djangotest/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from core.models import TimeStamp
-
-class User(TimeStamp):
-    email    = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=100, null=True)
-    
-    class Meta:
-        db_table = 'users'
