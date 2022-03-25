2fccbe71cf0f6766901528c7c93a87fa62255fdd
song <song7868@gmail.com>
2022-03-25 18:21:06 +0900

    FIX : 3장 ex03.py

--- TIL/python/inflearn/기초부터_실무까지-part1/3장(자료형/ex03.py
+++ TIL/python/inflearn/기초부터_실무까지-part1/3장(자료형/ex03.py
@@ -11,3 +11,24 @@ coin_100 = int(input("100원 동전 갯수 입력 : "))
 
 # 거스름돈 구하기
 nod_money = ((bills_1000 * 1000) + (coin_500 * 500) + (coin_100 * 100)) - itemPrice
+print("거스름돈 : ", nod_money)
+# 거스름돈(500원 동전 갯수)를 계산
+nCoin500 = nod_money // 500
+nod_money = nod_money % 500 #500원으로 나눈 나머지 값
+
+# 거스름돈(100원 동전 갯수)를 계산
+nCoin100 = nod_money // 100
+nod_money = nod_money % 100 #100원으로 나눈 나머지 값
+
+# 거스름돈(50원 동전 갯수)를 계산
+nCoin50 = nod_money // 50
+nod_money = nod_money % 50 #50원으로 나눈 나머지 값
+
+# 거스름돈(10원 동전 갯수)를 계산
+nCoin10 = nod_money // 10
+nod_money = nod_money % 10 #10원으로 나눈 나머지 값
+
+# 거스름돈(1원 동전 갯수)를 계산
+nCoin1 = nod_money
+
+print("500원 갯수 : ", nCoin500, "\n100원 갯수 : ", nCoin100, "\n50원 갯수 : ", nCoin50, "\n10원 갯수 : ", nCoin10, "\n1원 갯수 : ", nCoin1)
