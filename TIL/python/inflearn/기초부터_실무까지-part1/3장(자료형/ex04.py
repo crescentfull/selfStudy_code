4c520d75ec64ab477daaa38c00f3948c63f96e9a
song <song7868@gmail.com>
2022-03-30 04:35:35 +0900

    ADD : ex04.py

---
+++ TIL/python/inflearn/기초부터_실무까지-part1/3장(자료형/ex04.py
@@ -0,0 +1,17 @@
+#문자열 실습
+#큰따옴표(double quotation)
+welcome = "Happy New Year 2021"
+print(welcome)  
+#작은따옴표(single quotation)로 묶어도 문자열이 된다.
+welcome = 'Happy New Year 2021'
+print(welcome)
+
+# "" 안에 ''를 이용하면 출력가능하다
+message = "a가 '안녕'이라고 말한다"
+print(message)
+
+# 파이썬에서는 따옴표를 출력하고자 할 때,\를 이용
+## \가 따옴표를 앞에 있으면 '는 특수한 의미를 잃어버리고 하나의 문자로 편승이 되어 문자열을 이룬다.
+message = 'doesn\'t'
+print(message)
+
