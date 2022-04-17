0c666955bbedf323b4b8a7a891498891cabba39d
song <song7868@gmail.com>
2022-04-17 14:41:13 +0900

    ADD : 5장/ex13.py

--- TIL/language/python/inflearn/기초부터_실무까지-part1/5장_반복문/ex13.py
+++ TIL/language/python/inflearn/기초부터_실무까지-part1/5장_반복문/ex13.py
@@ -4,6 +4,20 @@
 cnt = 0
 sum = 0
 score = 0
-
+avr = 0.0
 #센티넬 (보초값)을 사용자에 제시하는 코드
 print("종료하시려면 음수를 입력하세요.(예 : -1) :")
+while score >= 0:
+    score = int(input(str(cnt+1)+"번째 학생의 성적을 입력하세요. : "))
+    # 음수 값인지 구하는 코드
+    if score >= 0:
+        sum += score #성적의 합계를 구하는 코드
+        cnt += 1     #학생의 수를 누적하는 코드
+        
+#합계와 평균을 계산하고 출력하는 코드
+if cnt > 0:
+    aver = sum/cnt
+
+# 합계와 평균을 출력하는 코드
+print(str(cnt+1)+"학생의 평균은 %0.1f입니다" %avr)
+    
