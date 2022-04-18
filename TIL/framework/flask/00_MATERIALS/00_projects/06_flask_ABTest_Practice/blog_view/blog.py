d0448c5ff038f16c6be6414a213673cf9600e4ac
song <song7868@gmail.com>
2022-04-19 00:10:59 +0900

    delete

--- TIL/framework/flask/00_MATERIALS/00_projects/06_flask_ABTest_Practice/blog_view/blog.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, Blueprint, request, render_template
-
-blog_abtest = Blueprint('blog', __name__)
-
-
-@blog_abtest.route('/test')
-def test():
-    return render_template('blog_A.html')
--- TIL/framework/flask/00_MATERIALS/00_projects/10_test/db_model/test_db_model.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def test_db():
-    print(os.path.dirname(os.path.realpath(__file__)) )
-    print('db_model', 'test_db')
