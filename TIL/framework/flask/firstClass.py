0fb056405b15eab1b4b6d0b64e6402a950a78553
song <song7868@gmail.com>
2022-04-23 23:07:30 +0900

    ADD : firstClass.py

---
+++ TIL/framework/flask/firstClass.py
@@ -0,0 +1,9 @@
+def list_creator(tag):
+    def text_wrapper(msg):
+        print('<{0}>{1}</{0}>'.format(tag,msg))
+    return text_wrapper
+
+ul_list_creator = list_creator('ul')
+print(ul_list_creator)
+
+ul_list_creator("목차")
