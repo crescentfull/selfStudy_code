2c4e31dc56f476e1bfd0bcaa069c0e8cd90d6b0a
song <song7868@gmail.com>
2022-04-25 23:46:42 +0900

    ADD : 5장/ex15.py

--- TIL/language/python/inflearn/기초부터_실무까지-part1/5장_반복문/ex15.py
+++ TIL/language/python/inflearn/기초부터_실무까지-part1/5장_반복문/ex15.py
@@ -1,9 +1,12 @@
 # 사용자로부터 상품금액을 입력받아서 상품의 총 가격을 계산하는 프로그램 만들기
 # 사용자가 몇 개의 상품을 살지 모르니깐 무한루프를 이용하고, 사용자가 "끝"이라고 입력을 하면 루프를 종료하게 조건을 준다
-
-tot = 0
-price = 0
+from operator import eq
+total = 0
+price = ""
 
 while True:
-    print("상품 금액을 입력하세요.")
-    
+    price = input("상품 금액을 입력하세요.('끝' 입력하면 종료) : ")
+    if eq(price, "끝"):   # if price == "끝"이랑 동일. operator import해줘야한다.
+        print("상품의 총 가격 : "+str(total)+"원")
+        break
+    total += int(price)
