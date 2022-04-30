e6ad382cca8db883c03ce8ccb77dd445a59b1063
song <song7868@gmail.com>
2022-04-30 09:11:31 +0900

    ADD :

---
+++ TIL/framework/flask/inflearn/firstClass.py
@@ -0,0 +1,12 @@
+# def list_creator(tag):
+#     def text_wrapper(msg):
+#         print('<{0}>{1}</{0}>'.format(tag,msg))
+#     return text_wrapper
+
+# ul_list_creator = list_creator('ul')
+# print(ul_list_creator)
+
+# ul_list_creator("목차")
+
+def list_creator(tag):
+    
