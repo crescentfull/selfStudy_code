771c72c2ecdae6e9779477bf188b725dad1b2190
song <song7868@gmail.com>
2022-05-12 17:38:56 +0900

    ADD : 5장/ex20.py

--- TIL/language/python/inflearn/기초부터_실무까지-part1/5장_반복문/ex20.py
+++ TIL/language/python/inflearn/기초부터_실무까지-part1/5장_반복문/ex20.py
@@ -2,3 +2,27 @@
 # 마지막으로 더해지는 소수는 얼마인지를 출력
 # 중첩루프, 조건식 사용
 
+from tracemalloc import start
+
+
+start_num = 0
+num = 0
+sum = 0
+lastData = 0
+
+# for num in range(1,2001):
+#     if num%2 == 0:
+#         sum += num
+#     num+=1
+# print(sum)
+
+for num in range(2,2001):
+    for start_num in range(2,num+1):
+        if num % start_num == 0:
+            break 
+    if num == start_num:
+        print("소수  : ", start_num)
+        sum += start_num
+        print("합계 : ", sum)
+        lastData = start_num
+        print("마지막 소수 : ", lastData)
