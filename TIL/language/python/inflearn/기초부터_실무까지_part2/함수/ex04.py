1a6ef59123c0cfaeee4466cb602fc92307976b35
song <song7868@gmail.com>
2022-06-01 18:48:04 +0900

    ADD : 함수/ex05.py

--- TIL/language/python/inflearn/기초부터_실무까지_part2/함수/ex04.py
+++ TIL/language/python/inflearn/기초부터_실무까지_part2/함수/ex04.py
@@ -2,12 +2,14 @@
 # 공식 : 화씨(F) = (9.0/5.0)*섭씨(C)+32
 
 def main():
-    print("화씨",temp_f,"를 섭씨 온도로 변환한 값",round(FtoC(temp_f)))
+    print("화씨",temp_f,"를 섭씨 온도로 변환한 값",round(FtoC(temp_f),2))
+    
+temp_f = float(input("온도를 입력해 주세요 : "))
 #함수 선언 및 구현
 def FtoC(temp_f):
     temp_c = (5.0 * (temp_f - 32.0))/9.0
     return temp_c
 
-temp_f = float(input("온도를 입력해 주세요 : "))
+main()
 #함수 호출
-print("화씨 : ", temp_f,"섭씨 : ", round(FtoC(temp_f),2))
+# print("화씨 : ", temp_f,"섭씨 : ", round(FtoC(temp_f),2))
