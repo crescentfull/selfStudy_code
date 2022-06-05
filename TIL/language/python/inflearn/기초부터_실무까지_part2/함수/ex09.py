7d461ff25f19219dc3b424233e2b3516d6a9cdf0
song <song7868@gmail.com>
2022-06-06 00:51:35 +0900

    ADD : 함수/ex09.py

--- TIL/language/python/inflearn/기초부터_실무까지_part2/함수/ex09.py
+++ TIL/language/python/inflearn/기초부터_실무까지_part2/함수/ex09.py
@@ -4,7 +4,7 @@
 def cal(x,y,z):
     return x-y+z
 
-print(cal(10,100,1000)) # 기본
+print(cal(10,100,1000)) # 기본 위치 인수(positional argument)
 print(cal(z=10,x=100,y=1000)) # 키워드 인수
 
 #! 장점 : 매개변수의 위치에 상관없이 직접 키워드 값을 할당함으로써 함수를 호출할수 있다.
