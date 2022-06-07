6bed3bc3a6f13a824177609eff779c4b372d0d14
song <song7868@gmail.com>
2022-06-08 05:01:26 +0900

    ADD : quadrangle.py

--- TIL/language/python/class/quadrangle.py
+++ TIL/language/python/class/quadrangle.py
@@ -1,2 +1,22 @@
 class Quadrangle:
+    def __init__(self, width, height, color):
+        self.width  = width
+        self.height = height
+        self.color  = color
+        
+    def get_area(self):
+        return self.width * self.height
     
+    def set_area(self, data1, data2):
+        self.width  = data1
+        self.height = data2
+
+sqaure1 =  Quadrangle(1, 2, 'blue')
+sqaure2 =  Quadrangle(3, 4, 'red')
+sqaure1.set_area(10, 5)
+sqaure2.set_area(7, 7)
+sqaure1.color = 'red'
+sqaure1.color = 'blue'
+
+print(sqaure1.get_area())
+print(sqaure2.get_area())
