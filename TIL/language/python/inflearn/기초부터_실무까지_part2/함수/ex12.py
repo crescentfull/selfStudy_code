68eed3490a7b386f7840ee7b850293d239d94545
song <song7868@gmail.com>
2022-06-09 23:59:53 +0900

    ADD : ex12.py

--- TIL/language/python/inflearn/기초부터_실무까지_part2/함수/ex12.py
+++ TIL/language/python/inflearn/기초부터_실무까지_part2/함수/ex12.py
@@ -1,3 +1,18 @@
 # 사용자로부터 10진수를 입력받아서 2진수 문자열로 변환하는 반환하는 decTobin(num)을 작성
 def decTobin(num):
-    return
+    binary = ''
+    
+    while num != 0:
+        value = (num%2)==0
+        if value == 0:
+            binary = '0' + binary
+        else:
+            binary = '1' + binary
+        num = num//2
+        print(num)
+    return print(binary)
+decTobin(12)
+
+print(bin(12)) #2진법
+oct(12) #8진법
+hex(12) #16진법
