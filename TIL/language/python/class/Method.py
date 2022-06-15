e61b1d54cba331c8d28e546bd2c879d08f25c762
song <song7868@gmail.com>
2022-06-16 05:08:15 +0900

    ADD : python/class/method.py

--- TIL/language/python/class/Method.py
+++ TIL/language/python/class/Method.py
@@ -7,3 +7,24 @@
 # - 객체 속성에 접근이 불가하다
 # - 정적 메서드는 메서드 앞에 @staticmethod 라는 decorator를 넣어줘야한다
 # 클래스명.정적메서드명 또는 객체명.정적메서드명 둘다 호출이 가능하다
+
+from turtle import width
+
+
+class Figure:
+    #생성자(initializer)
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+    #메서드
+    def clac_area(self):
+        return self.width * self.height
+    
+    #정적메서드(Figure에 너비와 높이가 같은 도형은 정사각형임을 알려주는 기능)
+    @staticmethod
+    def is_square(rect_width, rect_height):
+        if rect_width == rect_height:
+            print("정사각형")
+        else:
+            print("no 정사각형")
