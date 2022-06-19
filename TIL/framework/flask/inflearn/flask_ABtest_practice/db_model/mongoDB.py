6b507e63dac7a431ab34eb63867e23a7cebdd630
song <song7868@gmail.com>
2022-06-19 17:57:30 +0900

    FIX : mongoDB.py connection setting

--- TIL/framework/flask/inflearn/flask_ABtest_practice/db_model/mongoDB.py
+++ TIL/framework/flask/inflearn/flask_ABtest_practice/db_model/mongoDB.py
@@ -2,3 +2,13 @@ import pymongo
 import config
 
 MONGO_HOST = config.DB_CONFIG['host']
+MONGO_CONN = pymongo.MongoClient('mongodb://%s' % (MONGO_HOST))
+
+def conn_mongo():
+    try:
+        MONGO_CONN.admin.command(config.DB_CONFIG['user'])
+        blog_ab = MONGO_CONN.blog_session_db.blog_ab
+    except:
+        MONGO_CONN = pymongo.MongoClient('mongodb://%s' % (MONGO_HOST))
+        blog_ab = MONGO_CONN.blog_session_db.blog_ab
+    return blog_ab
