51445425f6db44c4c80e29efe1bac095654c064f
song <song7868@gmail.com>
2022-06-25 18:04:23 +0900

    FIX : blog.py method GET,POS add

--- TIL/framework/flask/inflearn/flask_ABtest_practice/blog_view/blog.py
+++ TIL/framework/flask/inflearn/flask_ABtest_practice/blog_view/blog.py
@@ -1,7 +1,24 @@
-from flask import Flask, Blueprint, request, render_template
+from crypt import methods
+from sre_constants import SUCCESS
+from flask import Flask, Blueprint, request, render_template, make_response, jsonify, redirect, url_for
 
 blog_abtest = Blueprint('blog', __name__)
 
+
+@blog_abtest.route('/set_email', methods=['GET','POST'])
+def set_email():
+    if request.method == 'GET':
+        print('set_email : ', request.headers)
+        print('set_email : ', request.args.get('user_email'))
+        return redirect(url_for('blog.test')) # 1
+    else:
+        print('set_email : ', request.headers)
+        #print('set_email : ', request.get_jso n()) # content type이 application/json인 경우
+        print('set_email : ', request.form['user_email'])
+        return redirect(url_for('blog.test'))
+    # return redirect('/blog/test') # 2
+    # 1,2번중 더 편하고 적합한 것으로 사용하면 된다.
+    # return make_response(jsonify(SUCCESS=True), 200)
 
 @blog_abtest.route('/test')
 def test():
