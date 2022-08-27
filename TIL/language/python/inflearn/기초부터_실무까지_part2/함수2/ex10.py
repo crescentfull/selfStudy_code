30cd92e1281e4368204e3bbbc6e2c1c1d91ee4f4
song <song7868@gmail.com>
2022-08-27 22:41:29 +0900

    ADD : ex10.py

--- TIL/language/python/inflearn/기초부터_실무까지_part2/함수2/ex10.py
+++ TIL/language/python/inflearn/기초부터_실무까지_part2/함수2/ex10.py
@@ -5,11 +5,23 @@ PI = 3.141592
 
 # 원의 면적을 구하는 함수를 선언 및 구현
 # 원의 면적 공식 : PI * 반지름의 제곰
-
+def main():
+    radius = float(input("반지름을 입력하시오 : "))
+    print("반지름이",radius,"인 원의 면적은 : ", circleArea(radius))
+    print("반지름이",radius,"인 원의 둘레는 : ", circleCircumference(radius))
+    
 def circleArea(radius):
     return PI * radius * radius
 
 print(circleArea(8))
 
-def circleCircumference(radius):
+def circleCircumference(radius): #둘레
     return 2 * PI * radius
+
+# radius = float(input("반지름을 입력하시오 : "))
+# print("반지름이",radius,"인 원의 면적은 : ", circleArea(radius))
+# print("반지름이",radius,"인 원의 둘레는 : ", circleCircumference(radius))
+
+#위의 식도 함수로 만들수 있다.. 당연!
+
+main() # 프로그램 시작을 알리는 함수 호출
