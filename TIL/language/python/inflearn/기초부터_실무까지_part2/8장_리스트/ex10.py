5422adb7b877661e3cd1df369239cceaae4b6c6e
song <song7868@gmail.com>
2022-10-08 23:09:27 +0900

    FIX : ex10.py

---
+++ TIL/language/python/inflearn/기초부터_실무까지_part2/8장_리스트/ex10.py
@@ -0,0 +1,13 @@
+# 값으로 호출, 참조로 호출의 차이점
+
+
+def func(x):
+    print("x = ", x,"address = ", id(x))
+    x += "하세요"
+    print("x = ",x, "address = ", id(x))
+
+y = 10 #정수형(변경 불가능한 객체)
+y = "안녕" #문자열(변경불가 객체)
+print("y = ", y, "address = ", id(y))
+func(y) # 함수 호출(값)
+print("y = ",y,"address = ",id(y))
