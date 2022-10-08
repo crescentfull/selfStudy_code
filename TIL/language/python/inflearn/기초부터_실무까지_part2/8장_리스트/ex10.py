c532521189dac00d9ec209a55a4f18afceb796e6
song <song7868@gmail.com>
2022-10-08 23:52:52 +0900

    FIX : ex10.py 값,참조 호출 주소값 차이점

--- TIL/language/python/inflearn/기초부터_실무까지_part2/8장_리스트/ex10.py
+++ TIL/language/python/inflearn/기초부터_실무까지_part2/8장_리스트/ex10.py
@@ -11,3 +11,15 @@ y = "안녕" #문자열(변경불가 객체)
 print("y = ", y, "address = ", id(y))
 func(y) # 함수 호출(값)
 print("y = ",y,"address = ",id(y))
+
+print("----------------------------------")
+def func_list(x):
+    print("x = ", x,"address = ", id(x))
+    x.append("하세요")
+    print("x = ",x, "address = ", id(x))
+
+y = [10,20,30] #정수형(변경 불가능한 객체)
+print("y = ", y, "address = ", id(y))
+func(y) # 함수 호출(값)
+print("y = ",y,"address = ",id(y))
+## id 값 같다.
