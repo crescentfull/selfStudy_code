980f1245304fbcf4eea7f255a152e97705b28223
song <song7868@gmail.com>
2022-10-24 19:55:41 +0900

    ADD : NLP_tutorial no.1

---
+++ TIL/language/python/NLP_tutorial/tutorial1.py
@@ -0,0 +1,14 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+text = "Song is person. Michle is man."
+doc = nlp(text)
+
+for sentence in doc.sents:
+    print(sentence)
+    for word in sentence:
+        print(word)
+        
+import nltk
+from nltk.tokenize import
