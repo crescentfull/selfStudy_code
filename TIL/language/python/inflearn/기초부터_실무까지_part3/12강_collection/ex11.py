0cc748d38a143434f25062bf652c9c7e2febac62
song <song7868@gmail.com>
2022-12-06 18:54:10 +0900

    ADD : ex11.py namedtuple

---
+++ TIL/language/python/inflearn/기초부터_실무까지_part3/12강_collection/ex11.py
@@ -0,0 +1,41 @@
+# namedtuple 모듈은 튜플의 형태로 데이터를 구조화에 맞게끔
+# 저장하는 자료구조이다
+
+# 일반적인 튜플의 경우
+person1 = ("송영록", 14, "남")
+person2 = ("송주현", 11, "여")
+
+for i in [person1,person2]:
+    # %n이 들어가면서 튜플의 값들을 각각에 형식지정자에 맞게 적용
+    print("%s은(는) %d세의 %s성입니다." %i)
+
+print("-"*30)
+# namedtuple 인 경우
+from collections import namedtuple
+# person 이라는 namedtuple을 정의
+person = namedtuple(typename="person", field_names=('name age gender addr'))
+p1 = person(name='김연아', age=32, gender='여', addr='d')
+p2 = person(name='손연재', age=28, gender='여', addr='d')
+for n in [p1,p2]:
+    print("%s은(는) %d세의 %s성입니다. 사는도시 %s" %n)
+
+# person 이라고 정의를 해놓았고 그의 필드들이 3개가 존재하고 있기 때문에 
+# namedtuple은 (접근연산자)를 이용할 수도 있다.
+print(p1.name, p1.age, p1.gender,p1.addr)
+print(p2[0],p2[1],p2[2],p2[3])
+
+print("-"*30)
+# namedtuple의 _make() 메소드 : 기존에 생선된 namedtuple에 # 새로운 인스턴스를 생서해주는 메서드
+p3 = person._make(['송영록',15,'남','대구'])
+print(p3)
+
+for n in [p1,p2,p3]:
+    print("%s은(는) %d세의 %s성입니다. 사는도시 %s" %n)
+    
+p1 = p1._replace(name='강백호')
+p2 = p2._replace(age=44)
+p3 = p3._replace(addr='서울')
+print("-"*30)
+
+for n in [p1,p2,p3]:
+    print("%s은(는) %d세의 %s성입니다. 사는도시 %s" %n)
