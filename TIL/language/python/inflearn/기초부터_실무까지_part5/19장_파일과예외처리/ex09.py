9aede3e8be714690ad114bc1e5c87b8009edf5ee
song <song7868@gmail.com>
2023-01-03 23:47:17 +0900

    ADD : csv data 추출 활용

---
+++ TIL/language/python/inflearn/기초부터_실무까지_part5/19장_파일과예외처리/ex09.py
@@ -0,0 +1,12 @@
+import csv
+
+f = open("files/weather.csv", 'r')
+data = csv.reader(f)
+header = next(data) # header 제거하기
+temp = 1000
+
+for row in data:
+    if temp > float(row[3]): # 최저 기온은 3번 인덱스
+        temp = float(row[3])
+
+print('가장 추웠던 온도는? : ', temp)
