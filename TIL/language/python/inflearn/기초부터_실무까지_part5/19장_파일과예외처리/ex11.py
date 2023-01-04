ff968390a3b53418dda8b047fa77747125c126e0
song <song7868@gmail.com>
2023-01-04 15:42:09 +0900

    FIX : ex11.py file load

--- TIL/language/python/inflearn/기초부터_실무까지_part5/19장_파일과예외처리/ex11.py
+++ TIL/language/python/inflearn/기초부터_실무까지_part5/19장_파일과예외처리/ex11.py
@@ -1,14 +1,19 @@
-# 딕셔너리에 파일쓰기와 파일읽기
+# # 딕셔너리에 파일쓰기와 파일읽기
 import pickle
 
-# 딕셔너리를 만듬
-gameOption = {
-    "Sound" : 8,
-    "VideoQuality" : "HIGH",
-    "Money" : 100000,
-    "Weaponlist" : ["gun", "missile", "knife"]
-}
+# # 딕셔너리를 만듬
+# gameOption = {
+#     "Sound" : 8,
+#     "VideoQuality" : "HIGH",
+#     "Money" : 100000,
+#     "Weaponlist" : ["gun", "missile", "knife"]
+# }
 
-file = open("save.p", "wb")         # 이진 파일 오픈
-pickle.dump(gameOption, file)       # 딕셔너리를 피클 모듈을 이용하여 파일로 저장
+# file = open("save.txt", "wb")         # 이진 파일 오픈, 확장자명 마음대로 바꿔도 된다.
+# pickle.dump(gameOption, file)       # 딕셔너리를 피클 모듈을 이용하여 파일로 저장
+# file.close()
+
+file = open("save.p", "rb")
+obj = pickle.load(file)
+print(obj)
 file.close()
