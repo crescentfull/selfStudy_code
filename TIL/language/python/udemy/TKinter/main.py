d4c93c85fdb7f2be63f185bc1a0318cb559fcfcf
song <song7868@gmail.com>
2023-01-10 19:04:52 +0900

    FIX : main.py 레이아웃

--- TIL/language/python/udemy/TKinter/main.py
+++ TIL/language/python/udemy/TKinter/main.py
@@ -4,6 +4,8 @@ window = Tk()
 
 window.title("First GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=100, pady=20)
+
 
 # Label
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
@@ -11,6 +13,9 @@ my_label.pack() # side=left right bottom top , expend=
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text")
+# my_label.place(x=100, y=20) # 좌표설정 위치 마음대로 가능 ㅎ
+# my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 # Entry
 input = Entry(width=10)
