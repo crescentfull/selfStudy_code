5ba4af0a0b2f30aa57ce55f00db4184600b5b788
song <song7868@gmail.com>
2023-01-13 18:29:20 +0900

    FIX : reset function, count down marks set up

--- TIL/language/python/udemy/TKinter/pomodoro-start/main.py
+++ TIL/language/python/udemy/TKinter/pomodoro-start/main.py
@@ -7,11 +7,21 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text = "00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -25,13 +35,13 @@ def start_timer():
     
     if reps % 8 ==0:
         count_down(long_break_sec)
-        title_label.conig(text="break", fg=RED)
-    elif count_down(short_break_sec) % 2 == 0:
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
         count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK)
     else:
         count_down(work_sec)
-
-    
+        title_label.config(text="WORK", fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -44,7 +54,16 @@ def count_down(count):
     
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0: 
-        window.after(1000, count_down, count -1)
+        global timer
+        timer = window.after(1000, count_down, count -1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_marks.config(text=marks)
+            
     
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -68,11 +87,11 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="reset", highlightthickness=0)
+reset_button = Button(text="reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 # ! pack() grid() 같이 못쓴다.
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 
