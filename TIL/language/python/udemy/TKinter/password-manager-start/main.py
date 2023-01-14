09af8de774d297dffb5eda6834d7b13e2eae3128
song <song7868@gmail.com>
2023-01-15 01:12:47 +0900

    FIX : information save data as data.txt

--- TIL/language/python/udemy/TKinter/password-manager-start/main.py
+++ TIL/language/python/udemy/TKinter/password-manager-start/main.py
@@ -3,6 +3,16 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    
+    with open("data.txt", "a") as data_file:            #with -> file.close() 안해도 자동으로 열고 닫는다
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -15,8 +25,8 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # Labels
-websie_label = Label(text="Website")
-websie_label.grid(row=1, column=0)
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username")
 email_label.grid(row=2, column=0)
 password_label = Label(text="password")
@@ -24,7 +34,18 @@ password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
 password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Button
+generate_password_button = Button(text="Generate Password", width=10)
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="add", width=33, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
