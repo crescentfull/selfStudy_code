0a2471baed58ae1cfa24db5468b28dc2c044fbaf
song <song7868@gmail.com>
2023-01-14 23:01:58 +0900

    ADD : password-manager /Users/yeongroksong/opt/anaconda3/bin/python /Users/yeongroksong/Desktop/study/code/TIL/language/python/udemy/TKinter/password-manager-start/main.py

---
+++ TIL/language/python/udemy/TKinter/password-manager-start/main.py
@@ -0,0 +1,30 @@
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+websie_label = Label(text="Website")
+websie_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password")
+password_label.grid(row=3, column=0)
+
+# Entries
+website_entry = Entry(width=35)
+email_entry = Entry(width=35)
+password_entry = Entry(width=21)
+
+window.mainloop()
