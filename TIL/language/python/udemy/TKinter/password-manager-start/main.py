2022b0e3e0e50ac30b7b379f85258f39ddbce33e
song <song7868@gmail.com>
2023-01-16 03:48:26 +0900

    ADD : tkinter messagebox

--- TIL/language/python/udemy/TKinter/password-manager-start/main.py
+++ TIL/language/python/udemy/TKinter/password-manager-start/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -8,10 +9,16 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
     
-    with open("data.txt", "a") as data_file:            #with -> file.close() 안해도 자동으로 열고 닫는다
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="ERRORERROR", message="id 혹은 password에 빈공간이 있습니다.")
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                    f"\nPassword: {password} \nIs it OK to save?(y/n)")
+        if is_ok:
+            with open("data.txt", "a") as data_file:            #with -> file.close() 안해도 자동으로 열고 닫는다
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
         
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -38,7 +45,7 @@ website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-
+email_entry.insert(0, "sososo@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
