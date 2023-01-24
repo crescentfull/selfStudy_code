1f4a1e74981ad7bbc0583f9f8763ab066f1a9861
song <song7868@gmail.com>
2023-01-24 19:09:05 +0900

    ADD : try except else finally

--- TIL/language/python/udemy/session30/main.py
+++ TIL/language/python/udemy/session30/main.py
@@ -13,3 +13,19 @@
 # # TypeError
 # text = "abc"
 # print(text + 5)
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key" : "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("SOMETHING")
+except KeyError as error_msg:
+    print(f"the key {error_msg} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("file closed")
