812d2d8545f6d855d9aa029305201d9082b15bb6
song <song7868@gmail.com>
2023-01-31 00:03:52 +0900

    FIX : pyperclip

--- TIL/language/python/udemy/session30/Password+Manager/main.py
+++ TIL/language/python/udemy/session30/Password+Manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 #Password Generator Project
