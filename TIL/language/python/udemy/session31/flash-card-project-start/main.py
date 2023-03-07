4119c49140a8fc6a4af2307540d67b8ea5b4276f
song <song7868@gmail.com>
2023-03-07 23:26:15 +0900

    FIX : 카드 뒤집기 function

--- TIL/language/python/udemy/session31/flash-card-project-start/main.py
+++ TIL/language/python/udemy/session31/flash-card-project-start/main.py
@@ -20,6 +20,8 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window.after()
+
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263,image=card_front_img)
