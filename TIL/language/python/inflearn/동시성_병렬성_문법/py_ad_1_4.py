1b8c8aacb851404413fd7be3588e4e971d3657cb
song <song7868@gmail.com>
2023-03-13 22:58:19 +0900

    ADD : Thread(2)-Daemon, Join keyword - DaemonThread, Join
    DaemonThread(데몬스레드)

---
+++ TIL/language/python/inflearn/동시성_병렬성_문법/py_ad_1_4.py
@@ -0,0 +1,51 @@
+'''
+Sction 1
+Multithreading - Thread(2)-Daemon, Join
+Keyword - DaemonThread. Join
+'''
+'''
+
+DaemonThread(데몬스레드)
+    (1) 백그라운드 실행
+    (2) 메인스레드 종료시 즉시 종료(강제)
+    (3) 주로 백그라운드 무한 대기 이벤트 발생을 실행하는 부분을 담당 -> JVM(가비지 컬렉션), 자동 저장 등
+    (4) 일반 스레드는 작업 종료시 까지 실행
+    
+    
+'''
+import logging
+import threading
+import time
+
+# 스레드 실행 함수
+def thread_func(name):
+    logging.info("Sub-Thread %s: starting", name)
+    time.sleep(3) # 3sec
+    logging.info("Sub-Thread %s: finished", name)
+
+# 메인 영역
+if __name__ == "__main__":
+    # logging format 설정
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("Main-Thread: before creating thread")
+    
+    # 함수 인자 확인
+    x = threading.Thread(target=thread_func, args=('First',))
+    logging.info("Main-Thread: before running thread")
+    
+    # 서브 스레드 시작
+    x.start()
+    
+    # 주석 전후 결과 확인
+    # x.join()
+    # 자식 스레드가 종료될때까지 메인스레드 대기
+    
+    logging.info("Main-Thread: wait for the thread to finish")
+    
+    logging.info("Main-Thread: DONE")
+    
+'''
+메인 스레드가 종료되어도 서브스레드는 자신이 할일은 한뒤에 종료가 된다.
+(스레드는 한번 시작되면 완료되기 전까지 계속 진행된다.)
+'''
