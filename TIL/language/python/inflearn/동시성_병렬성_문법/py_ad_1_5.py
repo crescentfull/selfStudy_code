a7875c5702c07e861a41fb09c1abdb3cb3b0fb53
song <song7868@gmail.com>
2023-03-14 23:07:51 +0900

    ADD : Thread(3) - ThreadPoolExecutor keyword - Multiple Threads, concurrent.futures, (xxx)PoolExecutor

---
+++ TIL/language/python/inflearn/동시성_병렬성_문법/py_ad_1_5.py
@@ -0,0 +1,46 @@
+# thread 쓸때 유용한 Futures 패키지 
+'''
+Sction 1
+Multithreading - Thread(3) - ThreadPoolExecutor
+Keyword - Many Threads, concurrent.futures, (xxx)PoolExecutor
+'''
+'''
+
+그룹스레드
+    (1) python 3.2 이상 표준 라이브러리 사용
+    (2) concurrent.futures
+    (3) with 사용으로 생성, 소멸 라이프사이클 관리 용이
+    (4) 디버깅하기가 난해함(단점)
+    (5) 대기중인 작업 -> Queue -> 완료 상태조사 -> 결과 또는 예외 -> 단일화(캡슐화)    
+'''
+
+import logging
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+# 영역 3개
+def task(name):
+    logging.info(f'Sub-Thread {name}: starting')
+    
+    res = 0
+    for i in range(10001):
+        result = result + i
+    
+    logging.info(f'Sub-Thread {name}: finising result: {res}')
+    
+def main():
+    # logging format 설정
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    
+    logging.info('Main-Thread: before creating and running thread')
+
+    # 실행 방법 1
+    # max_worker: 작업의 갯수가 넘어가면 직접 설정이 유리
+    excutor = ThreadPoolExecutor(max_workers=3)
+    task1 = excutor.submit(task, ('First',))
+    task2 = excutor.submit(task, ('Second',))
+    task3 = excutor.submit(task, ('Third',))
+
+if __name__ == '__main__':
+    main()
