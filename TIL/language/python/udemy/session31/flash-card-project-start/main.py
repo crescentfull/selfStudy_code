38117eb980210e0113b747989945ad80764d95fc
song <song7868@gmail.com>
2023-03-21 10:09:44 +0900

    FIX : check event add is_known() when clicked known_button add data log file with pandas

--- TIL/language/python/udemy/session31/flash-card-project-start/main.py
+++ TIL/language/python/udemy/session31/flash-card-project-start/main.py
@@ -2,10 +2,17 @@ from tkinter import *
 import pandas, random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records") # data에서 뽑아온 정보를 dictionary로 변환 , 표 형식을 사용하기 위해 orient="records"를 이용 [{열:값, 열:값}, {열:값, 열:값}]
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 # -------- card 넘기기 --------
 def next_card():   
@@ -15,9 +22,9 @@ def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
-    canvas.itemconfig(card_background, iamge=card_front_img)
+    canvas.itemconfig(card_background, image=card_front_img)
     window.after(3000, func=flip_card)
 # ---------------------------
 def flip_card():
@@ -25,27 +32,36 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 # ---------------------------
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False) # csv 저장할때마다 index 지정 취소
+    next_card()
+# ---------------------------    
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 flip_timer = window.after(3000, func=flip_card)
 
+# 도안
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-card_background = canvas.create_image(400, 263,image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="word", font=("Arial", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(row=0, column=0, columnspan=2)
 
+# 이미지
 cross_image = PhotoImage(file="images/wrong.png") 
 unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
