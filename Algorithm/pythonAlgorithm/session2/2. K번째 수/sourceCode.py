71e02aae9fc067c59e846bcdab89dc07ef577b1f
song <song7868@gmail.com>
2023-03-31 15:51:36 +0900

    ADD : sourceCode

--- Algorithm/pythonAlgorithm/session2/2. K번째 수/sourceCode.py
+++ Algorithm/pythonAlgorithm/session2/2. K번째 수/sourceCode.py
@@ -1,3 +1,15 @@
+'''
+N개의 숫자로 이루어진 숫자열이 주어지면 해당 숫자열 중에서 s번째부터 e번째까지의 수를
+오름 차순 정렬했을 때 k번째로 나타나는 숫자를 출력하는 프로그램을 작성하세요.
+'''
+
 import sys
 sys.stdin=open("input.txt","rt")
 
+n,k = map(int, input().split())
+a = list(map(int, input().split()))
+print(n, k)
+print(a)
+a.sort()
+print(a)
+print(a[k-1])
