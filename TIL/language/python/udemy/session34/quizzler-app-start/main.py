a7b4a8fb8d3df030aea42b6e5f3069a988904fa7
song <song7868@gmail.com>
2023-04-05 15:44:24 +0900

    ADD : 퀴즈 앱 open Trivia DB를 이용한 퀴즈 앱 만들기

---
+++ TIL/language/python/udemy/session34/quizzler-app-start/main.py
@@ -0,0 +1,31 @@
+# from question_model import Question
+# from data import question_data
+# from quiz_brain import QuizBrain
+
+# question_bank = []
+# for question in question_data:
+#     question_text = question["question"]
+#     question_answer = question["correct_answer"]
+#     new_question = Question(question_text, question_answer)
+#     question_bank.append(new_question)
+
+
+# quiz = QuizBrain(question_bank)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+# print("You've completed the quiz")
+# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+import requests, json
+
+parameters = {
+    "amount" : 10,
+    "type" : "boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(json.dumps(data, indent=4))
