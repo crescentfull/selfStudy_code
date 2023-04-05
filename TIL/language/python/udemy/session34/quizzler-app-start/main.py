6645c852e95955320cf634538e8236dfb50602c6
song <song7868@gmail.com>
2023-04-05 17:12:57 +0900

    FIX : ui , data, quiz_brain data : api connect, data parsing quiz_brain : add html.unescape() ui : import tkinter

--- TIL/language/python/udemy/session34/quizzler-app-start/main.py
+++ TIL/language/python/udemy/session34/quizzler-app-start/main.py
@@ -1,31 +1,19 @@
-# from question_model import Question
-# from data import question_data
-# from quiz_brain import QuizBrain
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
 
-# question_bank = []
-# for question in question_data:
-#     question_text = question["question"]
-#     question_answer = question["correct_answer"]
-#     new_question = Question(question_text, question_answer)
-#     question_bank.append(new_question)
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
 
-# quiz = QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
 
-# while quiz.still_has_questions():
-#     quiz.next_question()
+while quiz.still_has_questions():
+    quiz.next_question()
 
-# print("You've completed the quiz")
-# print(f"Your final score was: {quiz.score}/{quiz.question_number}")
-
-import requests, json
-
-parameters = {
-    "amount" : 10,
-    "type" : "boolean"
-}
-
-response = requests.get("https://opentdb.com/api.php", params=parameters)
-response.raise_for_status()
-data = response.json()
-print(json.dumps(data, indent=4))
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
