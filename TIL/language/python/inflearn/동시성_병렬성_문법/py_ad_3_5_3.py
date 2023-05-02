c23e4fdc57471e3c4f1108650af3cd3e5a971305
song <song7868@gmail.com>
2023-05-02 18:04:34 +0900

    ADD : asyncio 비동기 sync, async / 동기, 비동기 프로그램 실행 비교

--- TIL/language/python/inflearn/동시성_병렬성_문법/py_ad_3_5_3.py
+++ TIL/language/python/inflearn/동시성_병렬성_문법/py_ad_3_5_3.py
@@ -11,3 +11,56 @@ Keyword - ascyncio
 
 '''
 
+import time 
+import asyncio
+
+# sync
+def exe_calculate_sync(name, n):
+    for i in range(1, n+1):
+        print(f'{name} -> {i} of {n} is calcualtiong.....')
+        time.sleep(1)
+    print(f'{name} : {n} working DONE!')
+
+def process_sync():
+    start = time.time()
+
+    exe_calculate_sync('one',3)
+    exe_calculate_sync('two',2)
+    exe_calculate_sync('one',1)
+    
+    end = time.time()
+    
+    print(f'>>> total seconds : {end - start}')
+    
+# async
+async def exe_calculate_async(name, n):
+    for i in range(1, n+1):
+        print(f'{name} -> {i} of {n} is calcualtiong.....')
+        await asyncio.sleep(1) #time.sleep(1)  ->> time 모듈은 비동기가 아니다. 이미 time 모듈안에서 def sleep() 이런식으로 동기로 설정되어 있기 때문이다. 따라서 await time.sleep()으로 사용 할 수 없다.
+    print(f'{name} : {n} working DONE!')
+
+async def process_async(): # 비동기로 실행 할때는 async를 붙인다. 
+    start = time.time()
+    
+    # 비동기 함수내에서 함수를 호출하려면 await을 붙인다
+    # 함수가 여러개이기 때문에 asyncio.wait([]) 리스트 함수로 묶어서 선언해준다.
+    await asyncio.wait([
+        exe_calculate_async('One', 3),
+        exe_calculate_async('Two', 2),
+        exe_calculate_async('Three', 1)
+    ]) 
+
+    
+    end = time.time()
+    
+    print(f'>>> total seconds : {end - start}')
+    
+if __name__ == '__main__':
+    # sync
+    #process_sync()
+    
+    # async
+    # python 3.7 이상
+    asyncio.run(process_async())
+    # 3.7 밑에 버전 
+    # asyncio.get_event_loop().run_until_complete(process_async())
