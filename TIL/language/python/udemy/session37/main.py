cad4e880993781f6cd6f0bc014ebde794523ba68
song <song7868@gmail.com>
2023-06-12 20:38:44 +0900

    FIX : change graph style cycling graph use header

--- TIL/language/python/udemy/session37/main.py
+++ TIL/language/python/udemy/session37/main.py
@@ -1,14 +1,34 @@
 # HTTP Requests
 import requests
 
+USERNAME = "test1231888"
+TOKEN = "jsifiidfn1in3in4ni5kkkdk"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token" : "jsifiidfn1in3in4ni5kkkdk",
-    "username" : "test1231888",
+    "token" : TOKEN,
+    "username" : TOKEN,
     "agreeTermsOfService" : "yes",
     "notMinor" : "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id" : "graph1",
+    "name" : "Cycling Graph",
+    "unit" : "Km",
+    "type" : "float",
+    "color" : "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response)
