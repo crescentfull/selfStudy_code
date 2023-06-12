2575a9140c18d59f75a40f22998241243927b284
song <song7868@gmail.com>
2023-06-12 17:28:47 +0900

    ADD : httpStatus practice with Pixela API

--- TIL/language/python/udemy/session37/main.py
+++ TIL/language/python/udemy/session37/main.py
@@ -1,1 +1,14 @@
 # HTTP Requests
+import requests
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token" : "jsifiidfn1in3in4ni5kkkdk",
+    "username" : "test1231888",
+    "agreeTermsOfService" : "yes",
+    "notMinor" : "yes",
+}
+
+response = requests.post(url=pixela_endpoint, json=user_params)
+print(response.text)
