f1dbbca8465242d17f8a7dbb0dc326c528e5ae87
song <song7868@gmail.com>
2023-06-21 23:33:59 +0900

    FIX : update, delete

--- TIL/language/python/udemy/session37/main.py
+++ TIL/language/python/udemy/session37/main.py
@@ -51,3 +51,15 @@ pixel_data = {
 # print(response.text)
 
 update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity" : "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
+print(response.text)
