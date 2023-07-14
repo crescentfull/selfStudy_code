25415eacca72317d40f79cb3a14aded6e0e9fb11
song <song7868@gmail.com>
2023-07-14 17:59:41 +0900

    FIX : solve

--- Algorithm/doit_알고리즘코딩테스트/배열,리스트/나머지_합_구하기.py
+++ Algorithm/doit_알고리즘코딩테스트/배열,리스트/나머지_합_구하기.py
@@ -12,5 +12,21 @@ N개의 수 A1, A2, ... An이 주어졌을때 연속된 부분의 합이 M으로
 7
 
 '''
-# 나머지 합 문제 풀이 핵심아이디어
-#(a+b)
+
+import sys
+input = sys.stdin.readline
+n,m = map(int, input().split())
+A = list(map(int, input().split()))
+S = [0] * n
+C = [0] * m
+answer = 0
+
+S[0] = A[0]
+
+for i in range(1, n):
+    S[i] - S[i-1] + A[i]
+
+for i in range(n):
+    remainder = S[i] % m
+    if remainder ==0:
+        answer += 1
