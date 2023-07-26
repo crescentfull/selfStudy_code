6e8e4f77b62d16efd9e29d39789bd9a56ef1dd92
song <song7868@gmail.com>
2023-07-26 17:27:05 +0900

    FIX :beautifulSoup article tag extrag

--- TIL/language/python/udemy/session45/bs4-start/main.py
+++ TIL/language/python/udemy/session45/bs4-start/main.py
@@ -3,6 +3,14 @@ import requests
 
 response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
+print(yc_web_page)
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-print(soup.title)
+print(soup)
+article_tag = soup.find(name="span", class_="titleline")
+print(article_tag)
+article_text = article_tag.getText()
+article_link = article_tag.get("href")
+article_upvote = soup.find(name="span", class_="score").getText()
+print(article_text)
+print(article_link)
