22f686cb842f020c76bf9bded8852e4b176bc921
song <song7868@gmail.com>
2023-08-02 20:39:37 +0900

    DONE : 폐기 웹크롤링은 openAPI와 html request만 써서 뽑아오는 것이아니다. 그저 스크래퍼일뿐, 진정한 크롤링에 대해서 고민해봐야한다. robots.txt

--- TIL/language/python/udemy/session46/main.py
+++ TIL/language/python/udemy/session46/main.py
@@ -1,1 +1,21 @@
-URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+import requests
+from bs4 import BeautifulSoup
+
+# URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+URL = "https://comic.naver.com/webtoon"
+
+response = requests.get(URL)
+response.raise_for_status()
+website_html = response.text
+# print(website_html)
+
+soup = BeautifulSoup(website_html, "lxml")
+
+all_movies = soup.find_all("ul", attrs={"class":"AsideList__content_list--FXDvm"})
+print(all_movies)
+
+movie_titles = [movie.getText() for movie in all_movies]
+print(movie_titles)
+
+# 안한다.
+# 제대로된 크롤링도 아니고 api 끌어다가 쓰는건데 정수를 배워보겠다
