7ce98bb8ac6fa3d3637b9839e5f17d0436f49ec2
song <song7868@gmail.com>
2023-09-01 23:27:06 +0900

    FIX : blog rendering connect header.html import error

--- TIL/language/python/udemy/session59/upgraded_blog/main.py
+++ TIL/language/python/udemy/session59/upgraded_blog/main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def get_all_posts():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5001")
