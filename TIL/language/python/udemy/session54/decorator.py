cee4f175172bc27daa9959846f0defe916a75d9e
song <song7868@gmail.com>
2023-10-28 23:43:32 +0900

    FIX :

--- TIL/language/python/udemy/session54/decorator.py
+++ TIL/language/python/udemy/session54/decorator.py
@@ -1,7 +1,28 @@
 # Python Decorator Function
+import time
 
-def decorator_function(function):
-    def wrapper_function():
-        function()
-    return wrapper_function
+# def decorator_function(function):
+#     def wrapper_function():
+#         function()
+#     return wrapper_function
 # 데코레이터란 단순히 다른 함수를 감싸서 추가기능을 부여하는 것이다.
+
+def delay_decorator(function):
+    def wrapper_function():
+        time.sleep(2)
+        #Do something before
+        function()
+        #Do something after
+    return wrapper_function
+
+@delay_decorator
+def say_hello():
+    time.sleep(2)
+    print("Hello")
+    
+@delay_decorator
+def say_bye():
+    print("Bye")
+    
+def say_greeting():
+    print("How are you?")
