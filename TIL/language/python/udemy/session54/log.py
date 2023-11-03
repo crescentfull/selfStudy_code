3c001e8247ca743a0727ded9fa6882a6408f557d
song <song7868@gmail.com>
2023-11-03 15:58:47 +0900

    FIX : log decorator sourcecode

--- TIL/language/python/udemy/session54/log.py
+++ TIL/language/python/udemy/session54/log.py
@@ -1,9 +1,14 @@
+inputs = eval(input("입력 : "))
+
 def logging_decorator(fn):
     def wrapper(*args):
-        
+        print(f"You called {fn.__name__}{args}")
+        result = fn(args[0],args[1],args[2])
+        print(f"It returned: {result}")
     return wrapper
 
+@logging_decorator
 def a_function(a, b, c):
     return a*b*c
 
-a_function(input[0],input[1],input[2])
+a_function(inputs[0],inputs[1],inputs[2])
---
+++ TIL/language/python/udemy/session54/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+inputs = eval(input("입력 :"))
