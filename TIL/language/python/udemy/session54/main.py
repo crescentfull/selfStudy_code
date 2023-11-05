380b1a2561b6b6095d867eaf8a31c0779dc3a8bc
song <song7868@gmail.com>
2023-11-05 23:47:32 +0900

    FIX : Higher-Lower URL sourceCode

--- TIL/language/python/udemy/session54/main.py
+++ TIL/language/python/udemy/session54/main.py
@@ -2,3 +2,31 @@ from flask import Flask
 import random
 
 random_number = random.randint(0, 9)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    print("랜덤 번호 : ", random_number)
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    print("랜덤 번호 : ", random_number)
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
