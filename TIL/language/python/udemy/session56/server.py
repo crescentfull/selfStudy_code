46547a460f3ddaf4c40ab8d95b3c326bd5cfdbc9
song <song7868@gmail.com>
2023-11-08 20:04:59 +0900

    FIX : html render in Flask test

--- TIL/language/python/udemy/session56/server.py
+++ TIL/language/python/udemy/session56/server.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-
-@app.route("/cv")
-def cv():
-    return render_template("cv.html")
     
 if __name__ == "__main__":
     app.run(debug=True)
