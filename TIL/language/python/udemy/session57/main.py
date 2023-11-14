ee02ae97bcc4fc94f9890affe3f5e18716be6965
song <song7868@gmail.com>
2023-11-14 20:23:56 +0900

    FIX : footer datetime 변수 삽입

--- TIL/language/python/udemy/session57/main.py
+++ TIL/language/python/udemy/session57/main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+    name = "SONG"
     random_number = random.randint(1,10)
-    return render_template("index.html", num = random_number)
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", 
+                           num = random_number,
+                           year = current_year,
+                           username = name)
 
 
 if __name__ == "__main__":
