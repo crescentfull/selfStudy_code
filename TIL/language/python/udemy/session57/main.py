8b029f2bac364866a4ddfb360fd0fae8833a9939
song <song7868@gmail.com>
2023-11-15 18:52:21 +0900

    ADD : api test

--- TIL/language/python/udemy/session57/main.py
+++ TIL/language/python/udemy/session57/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import random
 import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -15,6 +16,21 @@ def home():
                            year = current_year,
                            username = name)
 
-
+@app.route('/guess/<name>')
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+    return render_template("api.html",
+                           username = name,
+                           gender   = gender,
+                           age      = age)
+    
 if __name__ == "__main__":
     app.run(debug=True)
