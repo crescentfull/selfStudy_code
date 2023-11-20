3b8286fbeda853e7215506645bd6bb519f9af7d0
song <song7868@gmail.com>
2023-11-20 18:33:38 +0900

    FIX : post class 그래도 사용할때를 대비해서 class를 작성해보았다. main.py 에서 리스트로 받아줘야한다.

--- TIL/language/python/udemy/session57/blog-templating-start/post.py
+++ TIL/language/python/udemy/session57/blog-templating-start/post.py
@@ -1,6 +1,12 @@
 class Post:
-    def __init__(self, id, title, subtitle, body):
+    def __init__(self, id, title, body, subtitle):
         self.id = id
         self.title = title
-        self.subtitle = subtitle
         self.body = body
+        self.subtitle = subtitle
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.id}')"
+
+    # 필요한 경우 추가적인 메서드나 속성을 여기에 정의할 수 있습니다.
+    # __repr__ 메서드는 클래스의 문자열 표현을 정의합니다. 이는 디버깅할 때 유용할 수 있습니다
