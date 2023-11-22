9bcb26f76090c318d7830a646cb7dd4930c6e9e8
song <song7868@gmail.com>
2023-11-22 18:25:40 +0900

    FIX : HTML Methods form test

--- TIL/language/python/udemy/session60/main.py
+++ TIL/language/python/udemy/session60/main.py
@@ -1,14 +1,18 @@
-from flask import Flask, request, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route("/submit", methods=['POST'])
-def submit():
-    return request.method, 200
 
-if __name__ == '__main__':
+@app.route("/login", methods=["POST"])
+def receive_data():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name}, Password: {password}</h1>"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
