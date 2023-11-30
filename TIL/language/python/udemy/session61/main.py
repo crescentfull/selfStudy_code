ab5a1fa46590725f89f5d3e230bfa2092fddfe0e
song <song7868@gmail.com>
2023-11-30 22:05:14 +0900

    FIX : WTForms 유효성검사

--- TIL/language/python/udemy/session61/main.py
+++ TIL/language/python/udemy/session61/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label="Log In")
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ def home():
 @app.route("/login")
 def login():
     login_form = LoginForm()
+    login_form.validate_on_submit()
     return render_template('login.html', form=login_form)
 
 
