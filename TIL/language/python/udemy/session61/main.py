9e2826c18bfc1576c3a02e0c7bf6e98c1bba9eab
song <song7868@gmail.com>
2023-12-05 22:06:58 +0900

    FIX : WTForms 유효성검사 login.html, main.py fi x

--- TIL/language/python/udemy/session61/main.py
+++ TIL/language/python/udemy/session61/main.py
@@ -1,25 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
-    submit = SubmitField(label="Log In")
+from wtforms.validators import DataRequired, Email, Length
 
 app = Flask(__name__)
-app.secret_key = "rkfkrkfkdkwnrkfk"
+app.secret_key = "FormValidationKey_5641687416341687431"
+
+
+class LoginForm(FlaskForm):
+    name = StringField(label='Name')
+    email = StringField(label='Email', validators=[DataRequired(), Email(), Length(min=5)])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
+    submit = SubmitField(label="Log In", render_kw={"class": "btn btn-primary btn-lg"})
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "12345678":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template('login.html', form=login_form)
 
 
