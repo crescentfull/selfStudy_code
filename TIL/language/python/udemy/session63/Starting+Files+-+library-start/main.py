dc874119eb7aba7f3c713274c9e0d61f34b48aab
song <song7868@gmail.com>
2023-12-18 21:21:24 +0900

    FIX : POST function, index.html index.html 책 목록 반영

--- TIL/language/python/udemy/session63/Starting+Files+-+library-start/main.py
+++ TIL/language/python/udemy/session63/Starting+Files+-+library-start/main.py
@@ -4,14 +4,24 @@ app = Flask(__name__)
 
 all_books = []
 
-
 @app.route('/')
 def home():
-    return render_template("index.html")
+    print("all_books : ", all_books)
+    return render_template("index.html", books=all_books)
 
-
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        # new_book = {
+        #     "title" : request.form["title"],
+        #     "author" : request.form["author"],
+        #     "rating" : request.form["rating"]
+        # }
+        new_book = request.form.to_dict()
+        all_books.append(new_book)
+        print("new_book : ", new_book)
+        return redirect(url_for('home'))
+    
     return render_template("add.html")
 
 
