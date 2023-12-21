9d980ef590657cba6166518b684ccfdc4586acf0
song <song7868@gmail.com>
2023-12-21 22:43:57 +0900

    FIX : build CRUD

--- TIL/language/python/udemy/session63/Starting+Files+-+library-start/main.py
+++ TIL/language/python/udemy/session63/Starting+Files+-+library-start/main.py
@@ -1,8 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+# 데이터베이스 생성
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# 데이터베이스 생성
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
