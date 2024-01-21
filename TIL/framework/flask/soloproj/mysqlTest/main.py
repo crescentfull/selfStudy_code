7acbb28a25aaebc31907a9b2cc9dea74c695d4ce
song <song7868@gmail.com>
2024-01-21 22:14:22 +0900

    ADD : Flask with mysql test

---
+++ TIL/framework/flask/soloproj/mysqlTest/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+# MySQL 설정
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'yourusername'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'yourpassword'
+app.config['MYSQL_DATABASE_DB'] = 'yourdatabase'
+
+mysql = MySQL()
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    cursor = mysql.connect().cursor()
+    cursor.execute("SELECT * FROM your_table")
+    data = cursor.fetchall()
+    return str(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
