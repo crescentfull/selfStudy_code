68320ec68bd029e594bf58262783a72d899d33d6
song <song7868@gmail.com>
2024-01-25 17:33:02 +0900

    FIX : index 페이지 data load check

--- TIL/language/python/udemy/session64/Starting+Files+-+movie-project-start/app.py
+++ TIL/language/python/udemy/session64/Starting+Files+-+movie-project-start/app.py
@@ -15,6 +15,7 @@ def create_app():
         all_movies = Movie.query.all()
         return render_template("index.html", movie_list=all_movies)
 
+
     @app.route("/add", methods=["GET", "POST"])
     def add():
         form = MovieForm()
@@ -33,6 +34,19 @@ def create_app():
             return redirect(url_for('home'))
         return render_template("add.html", form=form)
 
+        # 데이터 추가 test
+        # new_movie = Movie(
+        #     title="Phone Booth",
+        #     year=2002,
+        #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+        #     rating=7.3,
+        #     ranking=10,
+        #     review="My favourite character was the caller.",
+        #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+        # )
+        # db.session.add(new_movie)
+        # db.session.commit()
+        # return redirect(url_for('home'))
     return app
 
 if __name__ == '__main__':
