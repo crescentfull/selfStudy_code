b622fc280c3f319a3bbdda2fcae0b1cd544aab27
song <song7868@gmail.com>
2024-02-01 19:04:05 +0900

    FIX : 특정 영화 클릭시 해당 영화 데이터 받아오기

--- TIL/language/python/udemy/session64/Starting+Files+-+movie-project-start/app.py
+++ TIL/language/python/udemy/session64/Starting+Files+-+movie-project-start/app.py
@@ -11,6 +11,9 @@ def create_app():
     app.config.from_object(Config)
     Bootstrap(app)
     db.init_app(app)
+    
+    MOVIE_DB_API_KEY = Config.MOVIE_DB_API_KEY
+    MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 
     @app.route("/")
     def home():
@@ -22,42 +25,30 @@ def create_app():
     def add_movie():
         form = FindMovieForm()
         if form.validate_on_submit():
-            MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
-            MOVIE_DB_API_KEY = Config.MOVIE_DB_API_KEY
             movie_title = form.title.data
             response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
             data = response.json()["results"]
             return render_template("select.html", options=data)
         return render_template("add.html", form=form)
-    # def add():
-        # form = MovieForm()
-        # if form.validate_on_submit():
-        #     new_movie = Movie(
-        #         title=form.title.data,
-        #         year=form.year.data,
-        #         description=form.description.data,
-        #         rating=form.rating.data,
-        #         ranking=form.ranking.data,
-        #         review=form.review.data,
-        #         img_url=form.img_url.data
-        #     )
-        #     db.session.add(new_movie)
-        #     db.session.commit()
-        #     return redirect(url_for('home'))
-        #         # 데이터 추가 test
-        # new_movie = Movie(
-        #     title="Phone Booth",
-        #     year=2002,
-        #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-        #     rating=7.3,
-        #     ranking=10,
-        #     review="My favourite character was the caller.",
-        #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-        # )
-        # db.session.add(new_movie)
-        # db.session.commit()
-        # return redirect(url_for('home'))
-        return render_template("add.html", form=form)
+    
+    @app.route("/find")
+    def find_movie():
+        MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+        MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+        movie_api_id = request.args.get("id")
+        if movie_api_id:
+            movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+            response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+            data = response.json()
+            new_movie = Movie(
+                title=data["title"],
+                year=data["release_date"].split("-")[0],
+                img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+                description=data["overview"]
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            return redirect(url_for("home"))
     
     @app.route("/update", methods=["GET","POST"])
     def update():
