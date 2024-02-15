e65ee31959e86cb7b6bbf4505b905dc07f4eaad2
song <song7868@gmail.com>
2024-02-15 15:52:33 +0900

    FIX : ckeditor 작동 CKEditorField의 데이터는 HTML로 저장되며, 여기에는 블로그 게시물의 모든 구조와 스타일이 포함됩니다. 블로그 게시물의 post.html 페이지로 이동했을 때 이러한 구조가 반영되도록 하려면 Jinja safe() filter를 추가해야 합니다. 이렇게 하면 진자(Jinja)가 post.html 템플릿을 렌더링할 때 HTML을 텍스트로 처리하지 않게 됩니다. 진자 필터를 적용하려면 파이프 기호 '|'가 필요하며, 이 기호는 진자 표현식과 진자 필터 사이에 들어갑니다. 예) {‌{ Jinja 표현식 | Jinja 필터 }}

--- TIL/language/python/udemy/session67/Starting+Files+-+RESTful-blog-start/main.py
+++ TIL/language/python/udemy/session67/Starting+Files+-+RESTful-blog-start/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -6,6 +6,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 
+from datetime import date
 
 ## Delete this code:
 # import requests
@@ -38,9 +39,8 @@ class CreatePostForm(FlaskForm):
     subtitle = StringField("Subtitle", validators=[DataRequired()])
     author = StringField("Your Name", validators=[DataRequired()])
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
-
 
 @app.route('/')
 def get_all_posts():
@@ -50,8 +50,32 @@ def get_all_posts():
 
 @app.route("/post/<int:post_id>")
 def show_post(post_id):
+    flash(f"show post {post_id}")
     requested_post = BlogPost.query.get(post_id)
     return render_template("post.html", post=requested_post)
+
+@app.route("/edit/<int:post_id>")
+def edit_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    post = request.form.get('body')
+    return render_template("make-post.html", post=requested_post)
+
+@app.route("/add/", methods=["GET", "POST"])
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=form.author.data,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
 
 
 @app.route("/about")
