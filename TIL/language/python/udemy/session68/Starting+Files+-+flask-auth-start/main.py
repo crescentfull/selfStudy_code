9e7815959188bf8ba0b3e9e9e6280e3e09ad6ac5
song <song7868@gmail.com>
2024-03-05 20:35:56 +0900

    FIX : hash encryption

--- TIL/language/python/udemy/session68/Starting+Files+-+flask-auth-start/main.py
+++ TIL/language/python/udemy/session68/Starting+Files+-+flask-auth-start/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, flash, sen
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 
