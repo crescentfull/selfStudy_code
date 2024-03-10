c08b35ca5b347d59e8c84c9d92ef63552778ca72
song <song7868@gmail.com>
2024-03-10 22:55:31 +0900

    FIX : hash encryption

--- TIL/language/python/udemy/session68/Starting+Files+-+flask-auth-start/main.py
+++ TIL/language/python/udemy/session68/Starting+Files+-+flask-auth-start/main.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-
+# login decorator
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
