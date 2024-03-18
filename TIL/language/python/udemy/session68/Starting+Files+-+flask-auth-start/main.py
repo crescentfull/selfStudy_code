8afb8491cd13c57522bfc747e9ffdbe1e611ef3a
song <song7868@gmail.com>
2024-03-18 17:49:06 +0900

    FIX : 인증상태 템플릿 전달

--- TIL/language/python/udemy/session68/Starting+Files+-+flask-auth-start/main.py
+++ TIL/language/python/udemy/session68/Starting+Files+-+flask-auth-start/main.py
@@ -30,18 +30,19 @@ class User(UserMixin, db.Model):
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-
-        if User.query.filter_by(email=request.form.get('email')).first():
-            #User already exists
+        email = request.form.get('email')
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
+        if user:
             flash("You've already signed up with that email, log in instead!")
             return redirect(url_for('login'))
-
         hash_and_salted_password = generate_password_hash(
             request.form.get('password'),
             method='pbkdf2:sha256',
@@ -49,15 +50,15 @@ def register():
         )
         new_user = User(
             email=request.form.get('email'),
-            name=request.form.get('name'),
             password=hash_and_salted_password,
+            name=request.form.get('name'),
         )
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
         return redirect(url_for("secrets"))
-
-    return render_template("register.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -65,29 +66,26 @@ def login():
     if request.method == "POST":
         email = request.form.get('email')
         password = request.form.get('password')
-    
-        user = User.query.filter_by(email=email).first()
-        #Email doesn't exist
+        result = db.session.execute(db.select(User).where(User.email == email))
+        user = result.scalar()
         if not user:
             flash("That email does not exist, please try again.")
             return redirect(url_for('login'))
-        #Password incorrect
         elif not check_password_hash(user.password, password):
             flash('Password incorrect, please try again.')
             return redirect(url_for('login'))
-        #Email exists and password correct
         else:
             login_user(user)
             return redirect(url_for('secrets'))
-
-    return render_template("login.html")
+    # Passing True or False if the user is authenticated. 
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route('/logout')
