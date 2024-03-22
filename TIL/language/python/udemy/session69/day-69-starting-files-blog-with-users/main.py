473f900df71f9fa92913cb108e38aeb3e21036b8
song <song7868@gmail.com>
2024-03-22 15:24:34 +0900

    FIX : db reengineer

--- TIL/language/python/udemy/session69/day-69-starting-files-blog-with-users/main.py
+++ TIL/language/python/udemy/session69/day-69-starting-files-blog-with-users/main.py
@@ -45,25 +45,29 @@ db.init_app(app)
 
 
 # CONFIGURE TABLES
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
-    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
-    date: Mapped[str] = mapped_column(String(250), nullable=False)
-    body: Mapped[str] = mapped_column(Text, nullable=False)
-    author: Mapped[str] = mapped_column(String(250), nullable=False)
-    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
-
-
-# TODO: Create a User table for all your registered users. 
+# 유저테이블
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100))
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
+    
+    posts = relationship("BlogPost", back_populates="author")
 
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    #Foreign key, "users.id"
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    #Create reference to the User object, the "posts" refers to the posts protperty in the User class.
+    author = relationship("User", back_populates="posts")
+    
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=True)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
 
 with app.app_context():
     db.create_all()
