cc772b55a5ae6fd1caa3c2adb0ab39573c4d2097
Yeongrok Song <song7868@gmail.com>
2025-04-03 19:20:51 +0900

    chore: service

---
+++ TIL/test/pytest/source/service.py
@@ -0,0 +1,9 @@
+# mocking
+database = {
+    1: "Alice",
+    2: "Bob",
+    3: "charlie"
+}
+
+def get_user_from_db(user_id):
+    return database.get(user_id)
---
+++ TIL/test/pytest/tests/test_service.py
@@ -0,0 +1,10 @@
+import pytest
+import source.service as service
+import unittest.mock as mock
+
+@mock.patch("source.service.get_user_from_db")
+def test_get_user_from_db(mock_get_user_from_db):
+    mock_get_user_from_db.return_value = "Mocked Alice"
+    user_name = service.get_user_from_db(1)
+
+    assert  user_name == "Mocked Alice"
